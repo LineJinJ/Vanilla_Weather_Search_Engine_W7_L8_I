2c00a86f34a333bff8877b0f64ba0bbb9d39f51b
LineJinJ <102582136+LineJinJ@users.noreply.github.com>
2022-06-16 18:10:57 +0200

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -43,9 +43,21 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "8469b2792b7396f5ad1a7f0ec5ad59c0";
-let city = "New York";
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}
+function search(city) {
+  let apiKey = "8469b2792b7396f5ad1a7f0ec5ad59c0";
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}
 &appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
